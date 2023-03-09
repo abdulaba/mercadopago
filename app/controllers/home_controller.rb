35b6250285942c3594e9153c82403bbfc267cc4e
andres ibanez <andresibanezulloa@gmail.com>
2023-03-09 10:43:53 -0300

    mercado pago rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def index
+    require 'mercadopago'
+   sdk = Mercadopago::SDK.new('')
+    # Crea un objeto de preferencia
+preference_data = {
+  items: [
+    {
+      title: 'Mi producto',
+      unit_price: 75,
+      quantity: 1
+    }
+  ]
+}
+preference_response = sdk.preference.create(preference_data)
+preference = preference_response[:response]
+
+# Este valor reemplazará el string "<%= @preference_id %>" en tu HTML
+@preference_id = preference['id']
+end
+
+end
