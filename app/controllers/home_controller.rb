b76b4dc08cd4df1ae4026626997df5e290715452
andres ibanez <andresibanezulloa@gmail.com>
2023-03-09 10:49:32 -0300

    identacion - key mercadopago

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,21 @@
 class HomeController < ApplicationController
   def index
     require 'mercadopago'
-   sdk = Mercadopago::SDK.new('')
+    sdk = Mercadopago::SDK.new('')
     # Crea un objeto de preferencia
-preference_data = {
-  items: [
-    {
-      title: 'Mi producto',
-      unit_price: 75,
-      quantity: 1
+    preference_data = {
+      items: [
+        {
+          title: 'Mi producto',
+          unit_price: 75,
+          quantity: 1
+        }
+      ]
     }
-  ]
-}
-preference_response = sdk.preference.create(preference_data)
-preference = preference_response[:response]
+    preference_response = sdk.preference.create(preference_data)
+    preference = preference_response[:response]
 
-# Este valor reemplazará el string "<%= @preference_id %>" en tu HTML
-@preference_id = preference['id']
-end
-
+    # Este valor reemplazará el string "<%= @preference_id %>" en tu HTML
+    @preference_id = preference['id']
+  end
 end
